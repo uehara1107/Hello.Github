0130c89b2d23225259c63e6253bff53b20b1e7cc
uehara1107 <u.sumika1107@gmail.com>
2019-07-04 17:51:51 +0900

    [add] FizzBazz

--- test.c
+++ test.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 int main(void)
 {
-  printf("test");
+  int i;
+
+  for(i = 1; i <= 15; i++){
+    if(i == 15){
+      printf("FizzBazz\n");
+    }
+    else if(i % 3 == 0){
+      printf("Fizz\n");
+    }
+    else if(i % 5 == 0){
+      printf("Bazz\n");
+    }
+    else {
+      printf("%d\n", i);
+    }
+  }
   
   return(0);
- }
+}
  
